bbc9d6d64482595f6cb4a62ab67b759dcbebd611
eguvenc <eguvenc@gmail.com>
2014-01-24 11:22:02 +0200

    update

---
+++ app/config/web_service.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+| -------------------------------------------------------------------
+| Web Service Package Configuration
+| -------------------------------------------------------------------
+| Build your rest service 
+| 
+| Prototype
+| -------------------------------------------------------------------
+| 
+| $web_service['service_folder'] = '/web_service';
+|
+*/
+
+$web_service['service_folder'] = '/web_service';
+
+
+/* End of file web_service.php */
+/* Location: .app/config/web_service.php */
