6f13c76be2c9c9147490200537ece304179c21da
silmag <burak.abir@gmail.com>
2014-03-15 17:47:54 +0200

    updates

--- public/welcome/controller/welcome.php
+++ public/welcome/controller/welcome.php
@@ -10,21 +10,12 @@ $c = new Controller(
         new Url;
         new Html;
         new View;
-<<<<<<< HEAD
-
-=======
->>>>>>> bb3a7dd6f6008cfe7b2909e673820f4c00960f7f
     }
 );
 
 $c->func(
     'index',
     function () {
-
-<<<<<<< HEAD
-=======
-        echo md5(rand(time()));
->>>>>>> bb3a7dd6f6008cfe7b2909e673820f4c00960f7f
 
         $this->view->get(
             'welcome', 
