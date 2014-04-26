69bf9dcf9dfeaeb32e760ee3ec6c966b9fbe8acb
eguvenc <eguvenc@gmail.com>
2014-04-26 21:03:39 +0300

    updates

---
+++ app/translations/tr_TR/lvc.php
@@ -0,0 +1,8 @@
+<?php
+
+// Model Turkish
+//-----------------------------------------------------------------
+
+$translate['e_404']    = '404 request not found.';
+$translate['success']  = 'Operation successful.';
+$translate['failure']  = 'We couldn\'t do operation at this time please try again.';
